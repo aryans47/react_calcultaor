d14c4912b1d02004c6f5b205fd2675d19cfcddf2
aryans47 <151876078+aryans47@users.noreply.github.com>
2024-03-04 18:34:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import moonIcon from "./assets/moon.png";
 import sunIcon from "./assets/sun.png";
 
 import "./App.css";
-
+//keycode stored in array which we used 
 const usedKeyCodes = [
   48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103,
   104, 105, 8, 13, 190, 187, 189, 191, 56, 111, 106, 107, 109,
@@ -26,37 +26,49 @@ function App() {
   );
 //handlekeypress function
   const handleKeyPress = (keyCode, key) => {
+    //if not keycode simply return
     if (!keyCode) return;
+    //if not usedkeycode simply return
     if (!usedKeyCodes.includes(keyCode)) return;
 
     if (numbers.includes(key)) {
       if (key === "0") {
         if (expression.length === 0) return;
       }
+      //real time chalculation
       calculateResult(expression + key);
+      //set expression
       setExression(expression + key);
-    } else if (operators.includes(key)) {
+    } 
+    //checking operator
+    else if (operators.includes(key)) {
       if (!expression) return;
-
+  //slice used for last character
       const lastChar = expression.slice(-1);
       if (operators.includes(lastChar)) return;
       if (lastChar === ".") return;
 
       setExression(expression + key);
-    } else if (key === ".") {
+    } 
+    // for checking decimal
+    else if (key === ".") {
       if (!expression) return;
       const lastChar = expression.slice(-1);
       if (!numbers.includes(lastChar)) return;
-
       setExression(expression + key);
-    } else if (keyCode === 8) {
+    } 
+    //8 is for backspace
+    else if (keyCode === 8) {
       if (!expression) return;
+      //eg=115 set to 11 real time backspace
       calculateResult(expression.slice(0, -1));
       setExression(expression.slice(0, -1));
-    } else if (keyCode === 13) {
+    } 
+    //13 for enter
+    else if (keyCode === 13) {
       if (!expression) return;
       calculateResult(expression);
-
+//history
       let tempHistory = [...history];
       if (tempHistory.length > 20) tempHistory = tempHistory.splice(0, 1);
       tempHistory.push(expression);
@@ -71,7 +83,6 @@ function App() {
     }
     const lastChar = exp.slice(-1);
     if (!numbers.includes(lastChar)) exp = exp.slice(0, -1);
-
     const answer = eval(exp).toFixed(2) + "";
     setResult(answer);
   };
